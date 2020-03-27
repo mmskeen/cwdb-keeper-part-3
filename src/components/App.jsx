00f563aed99326d0684c4aa56048566e19b6ab66
Michael Skeen <mmskeen@gmail.com>
2020-03-27 12:26:42 -0600

    completed simple notes app: delete, add, etc.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes([...notes, newNote]);
+  }
+  function deleteNote(id) {
+    setNotes(notes.filter((n, index) => id !== index));
+  }
+  const noteItems = notes.map((n, index) => (
+    <Note
+      key={index}
+      title={n.title}
+      content={n.content}
+      onDelete={() => deleteNote(index)}
+    />
+  ));
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {noteItems}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea({ onAdd }) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setNote(prevNote => ({ ...prevNote, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAdd(note);
+    setNote({ title: "", content: "" });
+  }
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          value={note.content}
+          rows="3"
+        />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
